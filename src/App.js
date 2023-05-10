f4eef6bad6fb5a6b8601d7f4f910f86a712550ca
Gudkov <gudkov.id@mail.ru>
2023-05-10 18:53:07 +0300

    feat: add basic markup, start: components script

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import moment from 'moment';
+import Header from "./header";
 
-function App() {
+import { data } from './data/mocks.js'
+
+export default function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [data, setData] = useState("");
+  const minDate = moment().format('YYYY-MM-DD')
+  
+  // const handlePastTime = (evt) => {
+  //   const selectedTime = moment(evt.target.value, 'HH:mm');
+  //   const currentTime = moment();
+  //   console.log(selectedTime)
+  //   if (selectedTime.isBefore(currentTime)) {
+  //     evt.target.setCustomValidity('Выберите корректное время')
+  //   } else {
+  //     evt.target.setCustomValidity('')
+  //   }
+  // }как сделать выбор времени от и до js
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+      <Header />
+      <select {...register("tower", { required: true })}>
+      <option value="" disabled selected  hidden> Выберите одну из наших башен </option>
+        <option value="A">Башня А</option>
+        <option value="B">Башня Б</option>
+      </select>
+      <select {...register("floor", { required: true })}>
+      <option value="" disabled selected  hidden> Выберите этаж </option>
+        <option value="3 floor">Этаж 3</option>
+        <option value="4 floor">Этаж 4</option> 
+        {/* добавить генератор этажей */}
+      </select>
+      <select {...register("room", { required: true })}>
+      <option value="" disabled selected  hidden> Выберите комнату </option>
+        <option value="room 1">Комната 1</option>
+        <option value="room 2">Комната 2</option> 
+        {/* добавить генератор комнат */}
+      </select>
+      <p>Выберите дату и время встречи:</p>
+      <input type="date" id="date"  min={minDate} {...register("date", { required: true })}/>
+      {errors.date && <span>Заполните это поле ДОБАВИТЬ СТИЛИ</span>}
+
+      {/* <input type="time" id="timeFrom" {...register("timeFrom", { required: true })} />
+      <input type="time" id="timeTo"  {...register("timeTo", { required: true })} /> */}
+      {errors.time && <div>Заполните это поле ДОБАВИТЬ СТИЛИ</div>}
+      <select {...register("time", { required: true })}>
+      <option value="" disabled selected  hidden> Выберите время </option>
+        <option time="1">08:00 - 09:00</option>
+        <option room="1">09:00 - 10:00</option>
+        <option room="1">10:00 - 11:00</option>
+        <option room="1">11:00 - 12:00</option>
+        <option room="1">12:00 - 13:00</option>
+        <option room="1">13:00 - 14:00</option>
+        <option room="1">14:00 - 15:00</option>
+        <option room="1">15:00 - 16:00</option>
+        <option room="1">16:00 - 17:00</option>
+        {/* добавить генератор комнат */}
+      </select>
+      <p>Дополните запрос при необходимости</p>
+      <textarea {...register("comment")} placeholder="Дополнительная информация" />
+      <p>{data}</p>
+      <input type="submit" onClick={() => {
+        
+        console.log({data})
+
+      }}/>
+      <input type='reset' />
+    </form>
   );
 }
-
-export default App;
---
+++ src/data/mocks.js
@@ -0,0 +1,56 @@
+export const data = [
+
+]
+
+const createDataArray = () => {
+  for (let i = 3; i <= 27; i++) {
+  let result =  {
+    floor: i,
+    rooms: [
+      {
+        number: 1,
+        isAvailible: true
+      },
+      {
+        number: 2,
+        isAvailible: false
+      },
+      {
+        number: 3,
+        isAvailible: false
+      },
+      {
+        number: 4,
+        isAvailible: true,
+      },
+      {
+        number: 5,
+        isAvailible: false
+      },
+      {
+        number: 6,
+        isAvailible: false
+      },
+       {
+        number: 7,
+        isAvailible: true
+      },
+      {
+        number: 8,
+        isAvailible: true
+      },
+      {
+        number: 9,
+        isAvailible: false
+      },
+      {
+        number: 10,
+        isAvailible: false
+      }
+    ]
+  }
+  data.push(result)
+    }
+  }
+  createDataArray()
+  console.log(data)
